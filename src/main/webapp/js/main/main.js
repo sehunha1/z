e79090d3dca9960298439069bd4a1c15e3e89f07
haseMac <hase@haseMac.local>
2017-02-09 11:12:23 +0900

    .

--- src/main/webapp/js/main/main.js
+++ src/main/webapp/js/main/main.js
@@ -1,5 +1,8 @@
 $(function() {
-$.get('../sidebar.html', function(result) {
-$('#sidebar').html(result);
-});
+  $.get("../../html/header.html", function(result) {
+    $("#header").html(result);
+  })
+  $.get("../../html/footer.html", function(result) {
+    $("#footer").html(result);
+  });
 })
