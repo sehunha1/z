d97720471089c7c62553617022119d05c44258c3
WooJong Kim <skimwooj@gmail.com>
2017-02-17 15:37:41 +0900

    sweetalert

--- src/main/webapp/html/auth/join.js
+++ src/main/webapp/html/auth/join.js
@@ -11,10 +11,22 @@ $('#add-btn').click(function() {
 			alert(ajaxResult.data);
 			return;
 		}
-		sweetAlert("회원가입 완료!", "......", "success")
+		//sweetAlert("회원가입 완료!", "......", "success")
 		//sweetAlert("회원가입 완료!", "로그인창으로 이동합니다.", "success");
 		//alert('회원가입이 완료되었습니다. 로그인창으로 이동합니다.');
 		//location.href = 'login.html';
+		
+		swal({
+			  title: "회원가입 완료!",
+			  text: "로그인 창으로 이동합니다.",
+			  type: "success",
+			},
+			function(){
+			  location.href ='login.html'
+			});
+		
+		
+		
 	}, 'json'); // post();
 
 }); // click() 
