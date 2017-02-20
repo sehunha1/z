94c7f3ddb5e84464d3aaec09128d2918056e4324
HeuikyungKim <hkkim00305@gmail.com>
2017-02-20 18:26:20 +0900

    .

---
+++ src/main/java/z/dao/BoardDao.java
@@ -0,0 +1,19 @@
+package z.dao;
+
+import java.util.ArrayList;
+
+import z.domain.Board;
+
+public interface BoardDao{
+  int count(String title) throws Exception;
+  int countByNo(int boardNo) throws Exception;
+  ArrayList<Board> getList() throws Exception;
+  int insert(Board board) throws Exception;
+  Board getOne(int BoardNo) throws Exception;
+  int update(Board board) throws Exception;
+  int delete(int boarNo) throws Exception;
+  
+
+
+}
+
