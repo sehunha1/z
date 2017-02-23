11ed25ef81e98b74f5beab67119978563e821f70
jned0317 <jned0317@naver.com>
2017-02-23 09:26:07 +0900

    .

--- src/main/java/z/domain/Link.java
+++ src/main/java/z/domain/Link.java
@@ -4,33 +4,40 @@ import java.io.Serializable;
 
 public class Link implements Serializable {
   private static final long serialVersionUID = 1L;
-  
+
   protected int memberNo;
   protected int meetingNo;
   protected String boss;
   protected String status;
-  
+
   public int getMemberNo() {
     return memberNo;
   }
+
   public void setMemberNo(int memberNo) {
     this.memberNo = memberNo;
   }
+
   public int getMeetingNo() {
     return meetingNo;
   }
-  public void setMeetingNo(String meetingNo) {
+
+  public void setMeetingNo(int meetingNo) {
     this.meetingNo = meetingNo;
   }
+
   public String getBoss() {
     return boss;
   }
+
   public void setBoss(String boss) {
     this.boss = boss;
   }
+
   public String getStatus() {
     return status;
   }
+
   public void setStatus(String status) {
     this.status = status;
   }
