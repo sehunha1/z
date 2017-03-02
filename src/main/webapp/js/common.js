8e6bf9f87d73c3956cd1f6fbe5ff4e1f1f90e2a7
jned0317 <jned0317@naver.com>
2017-03-02 15:07:02 +0900

    .

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,244 +1,293 @@
 // -------------------------페이스북------------------------//  
 
 // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      //document.getElementById('status').innerHTML = 'Please log ' +
-      //  'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      //document.getElementById('status').innerHTML = 'Please log ' +
-      //  'into Facebook.';
-    }
-  }
+function statusChangeCallback(response) {
+	console.log('statusChangeCallback');
+	console.log(response);
+	// The response object is returned with a status field that lets the
+	// app know the current login status of the person.
+	// Full docs on the response object can be found in the documentation
+	// for FB.getLoginStatus().
+	if (response.status === 'connected') {
+		// Logged into your app and Facebook.
+	} else if (response.status === 'not_authorized') {
+		// The person is logged into Facebook, but not your app.
+		// document.getElementById('status').innerHTML = 'Please log ' +
+		// 'into this app.';
+	} else {
+		// The person is not logged into Facebook, so we're not sure if
+		// they are logged into this app or not.
+		// document.getElementById('status').innerHTML = 'Please log ' +
+		// 'into Facebook.';
+	}
+}
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '{1343425079030171}',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.8' // use graph api version 2.8
-  });
-
-  /*
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-  */
-  
-  FB.getLoginStatus(function(response) {
-	    if (response.status === 'connected') {
-	        FB.api('/me/?fields=picture', function(user) {
-	            if (user) {
-	            	
-	            	/*
-	            	//var image = 'http://graph.facebook.com/' + user.id + '/picture';
-	            	var image = user.picture.data.url;
-	            	var imagesplit = image.split('/')[6].split('?')[0];
-	            	//var imagejpg = imagesplit[7].split('?')[0];
-	            	// replaceAll("\\?.*", "")
-	            	console.log(imagesplit);
-	            	//console.log(imagesplit[7].replace('/?/gi', ""));
-	            	
-	            	
-	            	//console.log(image);
-	            	//image.replace(/&/gi, '%26');
-	            	
-	            	//console.log(image.replace(/&/gi, '%26'));
-	            	
-	            	
-	            	// 사진 json
-	            	
-	            	$.getJSON('../auth/loginUser.json', function(ajaxResult) {
-	            		if (ajaxResult.data.photo != imagesplit) {
-	            			// 사진 DB에 추가
-	            			$.getJSON("../auth/updateFBphoto.json?filename=" + image.replace(/&/gi, '%26'), function(ajaxResult) {
-	    	                });
-	            			
-	            			// 사진 upload에 추가
-	            			$.getJSON("../../common/fbPhoto.json?filename=" + image.replace(/&/gi, '%26'), function(ajaxResult) {
-	    	                });
-	            		}
-	            		else {
-	            			$('#profile_photo').html('<img src="../upload/' + ajaxResult.data.photo + '" height=30px; width=30px;/>');
-	            		}
-	            	});
-	            	*/
-	                
-	            	
-	            	// 사진 json 끝
-	            	
-	            	
-	            	// image.src = 'http://graph.facebook.com/' + user.id + '/picture';
-	            	//document.getElementById('profile_photo').src = "yourpicture.png";
-	            	//$('#profile_photo').html('<img src="http://graph.facebook.com/' + user.id + '/picture" height=30px; width=30px;/>');
-	            	//$('#profile_photo').html('<img src="http://graph.facebook.com/' + user.id + '/picture" height=30px; width=30px;/>');
-	                //$('#inprofile_photo').html('<img src="http://graph.facebook.com/' + user.id + '/picture" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
-	    			//$('#inprofile_photo').html('<img src="../upload/' + image + '" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
-	                
-	                
-	            }
-	        });    
-	         
-	    } else if (response.status === 'not_authorized') {
-	        
-	    } else {
-	    
-	    }
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '{1343425079030171}',
+		cookie : true, // enable cookies to allow the server to access
+		// the session
+		xfbml : true, // parse social plugins on this page
+		version : 'v2.8' // use graph api version 2.8
 	});
 
-  };
+	/*
+	 * FB.getLoginStatus(function(response) { statusChangeCallback(response);
+	 * });
+	 */
 
-  // Load the SDK asynchronously
-	 (function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.8&appId=253788165032910";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {
+			FB.api('/me/?fields=picture', function(user) {
+				if (user) {
 
-	 
+					/*
+					 * //var image = 'http://graph.facebook.com/' + user.id +
+					 * '/picture'; var image = user.picture.data.url; var
+					 * imagesplit = image.split('/')[6].split('?')[0]; //var
+					 * imagejpg = imagesplit[7].split('?')[0]; //
+					 * replaceAll("\\?.*", "") console.log(imagesplit);
+					 * //console.log(imagesplit[7].replace('/?/gi', ""));
+					 * 
+					 * 
+					 * //console.log(image); //image.replace(/&/gi, '%26');
+					 * 
+					 * //console.log(image.replace(/&/gi, '%26'));
+					 * 
+					 *  // 사진 json
+					 * 
+					 * $.getJSON('../auth/loginUser.json', function(ajaxResult) {
+					 * if (ajaxResult.data.photo != imagesplit) { // 사진 DB에 추가
+					 * $.getJSON("../auth/updateFBphoto.json?filename=" +
+					 * image.replace(/&/gi, '%26'), function(ajaxResult) { });
+					 *  // 사진 upload에 추가
+					 * $.getJSON("../../common/fbPhoto.json?filename=" +
+					 * image.replace(/&/gi, '%26'), function(ajaxResult) { }); }
+					 * else { $('#profile_photo').html('<img src="../upload/' +
+					 * ajaxResult.data.photo + '" height=30px; width=30px;/>'); }
+					 * });
+					 */
+
+					// 사진 json 끝
+
+					// image.src = 'http://graph.facebook.com/' + user.id +
+					// '/picture';
+					// document.getElementById('profile_photo').src =
+					// "yourpicture.png";
+					// $('#profile_photo').html('<img
+					// src="http://graph.facebook.com/' + user.id + '/picture"
+					// height=30px; width=30px;/>');
+					// $('#profile_photo').html('<img
+					// src="http://graph.facebook.com/' + user.id + '/picture"
+					// height=30px; width=30px;/>');
+					// $('#inprofile_photo').html('<img
+					// src="http://graph.facebook.com/' + user.id + '/picture"
+					// style="width: 40px; height: 40px; margin-right: 12px;
+					// position: absolute;"/>');
+					// $('#inprofile_photo').html('<img src="../upload/' + image
+					// + '" style="width: 40px; height: 40px; margin-right:
+					// 12px; position: absolute;"/>');
+
+				}
+			});
+
+		} else if (response.status === 'not_authorized') {
+
+		} else {
+
+		}
+	});
+
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id))
+		return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.8&appId=253788165032910";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 // -------------------------페이스북------------------------//
 
-
-
-
-
-
-
-
 $(function() {
-    //헤더와 풋터 갖다붙이기
+	// 헤더와 풋터 갖다붙이기
 	/*
-  $.get("../../html/header.html", function(result) {
-    $("#header").html(result);
-  })*/
+	 * $.get("../../html/header.html", function(result) {
+	 * $("#header").html(result); })
+	 */
 	// header.html을 가져와서 붙인다.
 	$.get('../../html/header.html', function(result) {
-		// 서버에서 로그인 사용자 정보를 가져온다.
-		$.getJSON('../auth/loginUser.json', function(ajaxResult) {
-			$('#header').html(result);
+	// 서버에서 로그인 사용자 정보를 가져온다.
+	$.getJSON('../auth/loginUser.json', function(ajaxResult) {
+		$('#header').html(result);
 
-			if (ajaxResult.status == "fail") { // 로그인 되지 않았으면,
-				// 로그온 상태 출력 창을 감춘다.
-				$('#_login_status').css('display', 'none');
+		if (ajaxResult.status == "fail") { // 로그인
+											// 되지
+											// 않았으면,
+			// 로그온 상태 출력 창을 감춘다.
+			$('#_login_status').css(
+					'display', 'none');
 
-				// 로그인 버튼의 클릭 이벤트 핸들러 등록하기
-				$('#login-btn').click(function(event) {
-					event.preventDefault()
-					location.href = '../main/main.html'
-				});
-				return;
-			} else if (ajaxResult.status == "success") {
-				// 로그인 되었으면, 로그오프 상태 출력 창을 감춘다. 
-				$('#_logout_status').css('display', 'none');
-				//$('#sec-intext img').attr('src', '../upload/' + ajaxResult.data.photoPath);
-				//$('#nameo').text(ajaxResult.data.name);
-				$('#logout #name').text(ajaxResult.data.name);
-				$('#sec-intext #name').text(ajaxResult.data.name);
-				$('#sec-intext #emailin').text(ajaxResult.data.email);
-				
-				
-			   if (ajaxResult.data.photo == "") {
-					$('#profile_photo').html('<img src="../../image/profile-default.png" height=30px; width=30px;/>');
-					$('#inprofile_photo').html('<img src="../../image/profile-default.png" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
-				} else {
-				$('#profile_photo').html('<img src="../upload/' + ajaxResult.data.photo + '" height=30px; width=30px;/>');
-				$('#inprofile_photo').html('<img src="../upload/' + ajaxResult.data.photo + '" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
-				}
-			}
-      		
-		/*	// 로그인 되었으면, 로그오프 상태 출력 창을 감춘다. 
-			$('#_logout_status').css('display', 'none');
-			//$('#sec-intext img').attr('src', '../upload/' + ajaxResult.data.photoPath);
-			//$('#nameo').text(ajaxResult.data.name);
-			$('#logout #name').text(ajaxResult.data.name);
-			$('#sec-intext #name').text(ajaxResult.data.name);
-			$('#sec-intext #emailin').text(ajaxResult.data.email);
-			
+			// 로그인 버튼의 클릭 이벤트 핸들러 등록하기
+			$('#login-btn').click(function(event) {
+				event
+						.preventDefault()
+				location.href = '../main/main.html'
+			});
+			return;
+		} else if (ajaxResult.status == "success") {
+			// 로그인 되었으면, 로그오프 상태 출력 창을 감춘다.
+			$('#_logout_status').css(
+					'display', 'none');
+			// $('#sec-intext
+			// img').attr('src',
+			// '../upload/' +
+			// ajaxResult.data.photoPath);
+			// $('#nameo').text(ajaxResult.data.name);
+			$('#logout #name').text(
+					ajaxResult.data.name);
+			$('#sec-intext #name').text(
+					ajaxResult.data.name);
+			$('#sec-intext #emailin').text(
+					ajaxResult.data.email);
+
 			if (ajaxResult.data.photo == "") {
-				$('#profile_photo').html('<img src="../../image/profile-default.png" height=30px; width=30px;/>');
-				$('#inprofile_photo').html('<img src="../../image/profile-default.png" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
+				$('#profile_photo')
+						.html(
+								'<img src="../../image/profile-default.png" height=30px; width=30px;/>');
+				$('#inprofile_photo')
+						.html(
+								'<img src="../../image/profile-default.png" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
 			} else {
-			$('#profile_photo').html('<img src="../upload/' + ajaxResult.data.photo + '" height=30px; width=30px;/>');
-			$('#inprofile_photo').html('<img src="../upload/' + ajaxResult.data.photo + '" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
-			}*/
-			
-			//$('#profile_photo').text(ajaxResult.data.photo);
-			//$('#sec-intext #name').text(ajaxResult.data.name);
-			
-			
-			// 페이스북 API
+				$('#profile_photo')
+						.html(
+								'<img src="../upload/'
+										+ ajaxResult.data.photo
+										+ '" height=30px; width=30px;/>');
+				$('#inprofile_photo')
+						.html(
+								'<img src="../upload/'
+										+ ajaxResult.data.photo
+										+ '" style="width: 40px; height: 40px; margin-right: 12px; position: absolute;"/>');
+			}
+		}
 
+		/*
+		 * // 로그인 되었으면, 로그오프 상태 출력 창을 감춘다.
+		 * $('#_logout_status').css('display',
+		 * 'none'); //$('#sec-intext
+		 * img').attr('src', '../upload/' +
+		 * ajaxResult.data.photoPath);
+		 * //$('#nameo').text(ajaxResult.data.name);
+		 * $('#logout
+		 * #name').text(ajaxResult.data.name);
+		 * $('#sec-intext
+		 * #name').text(ajaxResult.data.name);
+		 * $('#sec-intext
+		 * #emailin').text(ajaxResult.data.email);
+		 * 
+		 * if (ajaxResult.data.photo == "") {
+		 * $('#profile_photo').html('<img
+		 * src="../../image/profile-default.png"
+		 * height=30px; width=30px;/>');
+		 * $('#inprofile_photo').html('<img
+		 * src="../../image/profile-default.png"
+		 * style="width: 40px; height: 40px;
+		 * margin-right: 12px; position:
+		 * absolute;"/>'); } else {
+		 * $('#profile_photo').html('<img
+		 * src="../upload/' +
+		 * ajaxResult.data.photo + '"
+		 * height=30px; width=30px;/>');
+		 * $('#inprofile_photo').html('<img
+		 * src="../upload/' +
+		 * ajaxResult.data.photo + '"
+		 * style="width: 40px; height: 40px;
+		 * margin-right: 12px; position:
+		 * absolute;"/>'); }
+		 */
 
-			// 로그아웃 버튼의 클릭 이벤트 핸들러 등록하기
-			$('#href_out').click(function(event) {
-				event.preventDefault();
-				$.getJSON('../auth/logout.json', function(ajaxResult) {
-					location.href = '../main/main.html'
-				});
+		// $('#profile_photo').text(ajaxResult.data.photo);
+		// $('#sec-intext
+		// #name').text(ajaxResult.data.name);
 
-				FB.logout(function (response) {
-			          //Do what ever you want here when logged out like reloading the page
-			          window.location.reload();
-			    });
+		// 페이스북 API
+
+		// 로그아웃 버튼의 클릭 이벤트 핸들러 등록하기
+		$('#href_out').click(function(event) {event.preventDefault();
+			$.getJSON('../auth/logout.json',function(ajaxResult) {
+								location.href = '../main/main.html'
+							});
+			FB.logout(function(response) {
+				// Do
+				// what
+				// ever
+				// you
+				// want
+				// here
+				// when
+				// logged
+				// out
+				// like
+				// reloading
+				// the
+				// page
+				window.location
+						.reload();
 			});
 		});
 	});
-  
-  $.get("../../html/footer.html", function(result) {
-    $("#footer").html(result);
-  })
-  
-    //헤더안의 유저프로필 클릭이벤트 발생
-  $("body").on("click", "#logout", function() {
-    if($("#userpopup").css("display") == "none") {
-      $("#userpopup").show();
-    }
-  })
-  $("body").on("click", function(e) {
-    var target = $(e.target);
-    if(target.parents("#logout").attr("id") !== "logout" && target.attr("class") !== "_layer") {
-      $("#userpopup").hide();
-    }
-  })
-  /*
-  $("body").on("click", "#login", function(e) {
-	  location.href = "../auth/login.html";
-  })
-  $("body").on("click", "#submit_btn", function(e) {
-    location.href = "afterlogin.html";
-  })*/
-  
-  $("body").on("click", "#href_mylist", function(e) {
-    e.preventDefault();
-    location.href = "../mylist/mylist.html";
-  })
-  
-    $("body").on("click", "#href_mypage", function(e) {
-    e.preventDefault();
-    location.href = "../mypage/main.html";
-  })
-  
-  /*
-  $("body").on("click", "#href_out", function(e) {
-    e.preventDefault();
-    location.href = "../main/main.html";
-  })*/
-  $("body").on("click", "#new-btn", function(e) {
-    e.preventDefault();
-    location.href = "write2.html";
-  });
+});
+
+	$.get("../../html/footer.html", function(result) {
+		$("#footer").html(result);
+	})
+
+	// 헤더안의 유저프로필 클릭이벤트 발생
+	$("body").on("click", "#logout", function() {
+		if ($("#userpopup").css("display") == "none") {
+			$("#userpopup").show();
+		}
+	})
+	$("body").on(
+			"click",
+			function(e) {
+				var target = $(e.target);
+				if (target.parents("#logout").attr("id") !== "logout"
+						&& target.attr("class") !== "_layer") {
+					$("#userpopup").hide();
+				}
+			})
+	/*
+	 * $("body").on("click", "#login", function(e) { location.href =
+	 * "../auth/login.html"; }) $("body").on("click", "#submit_btn", function(e) {
+	 * location.href = "afterlogin.html"; })
+	 */
+
+	$("body").on("click", "#href_mylist", function(e) {
+		e.preventDefault();
+		location.href = "../mylist/mylist.html";
+	})
+
+	$("body").on("click", "#href_mypage", function(e) {
+		e.preventDefault();
+		location.href = "../mypage/main.html";
+	})
+
+	/*
+	 * $("body").on("click", "#href_out", function(e) { e.preventDefault();
+	 * location.href = "../main/main.html"; })
+	 */
+	$("body").on("click", "#new-btn", function(e) {
+		e.preventDefault();
+		// 모임명 유효성 검사
+		if ($('#exampleInputEmail3').val().trim() == "") {
+			return false;
+		}
+		location.href = "write2.html?title=" + encodeURIComponent($('#exampleInputEmail3').val());
+	});
 })
--- src/main/webapp/js/main/write2.js
+++ src/main/webapp/js/main/write2.js
@@ -2,91 +2,88 @@
 // 모임 생성시 설정화면 javaScript
 // 작성: 2017.02.19 김재녕
 
+// 모임명 확인
+try {
+	var preTitle = decodeURIComponent(location.href.split('?')[1].split('=')[1]);
+} catch (error) {
+	var preTitle = "";
+}
+
+if (preTitle != "") {
+	$('#play-title').val(preTitle);
+}
+
 // GO 버튼 클릭시 데이터 저장 후 페이지 이동
-$("#Go-btn").click(
-		function(e) {
-			e.preventDefault();
+$("#Go-btn").click(function(e) {
+	e.preventDefault();
 
-			// 데이터 입력 여부 확인
-			if ($('#play-title').val().trim() == "") {
-				alert('모임명을 입력하세요.');
-				return;
-			} else if ($('#meeting-desc').val().trim() == "") {
-				alert('모임 분류를 선택하세요.');
-				return;
-			} else if ($('#limit-date').val().trim() == "") {
-				alert('투표 마감일을 선택하세요.');
-				return;
-			} else if ($('#possible-date').val().trim() == "") {
-				alert('투표 가능 기간을 선택하세요.');
-				return;
-			}
+	// 데이터 입력 여부 확인
+	if ($('#play-title').val().trim() == "") {
+		alert('모임명을 입력하세요.');
+		return;
+	} else if ($('#meeting-desc').val().trim() == "") {
+		alert('모임 분류를 선택하세요.');
+		return;
+	} else if ($('#limit-date').val().trim() == "") {
+		alert('투표 마감일을 선택하세요.');
+		return;
+	} else if ($('#possible-date').val().trim() == "") {
+		alert('투표 가능 기간을 선택하세요.');
+		return;
+	}
 
-			var dateList = $('#possible-date').val().split(" to ");
+	var dateList = $('#possible-date').val().split(" to ");
 
-			var param = {
-				"title" : $('#play-title').val(),
-				"category" : $('#meeting-desc').val(),
-				"deadline" : $('#limit-date').val(),
-				"sdate" : dateList[0],
-				"edate" : dateList[1],
-				"content" : $('#meeting-content').val(),
-				"photo" : $('#photo-path').val()
-			}
+	var param = {
+		"title" : $('#play-title').val(),
+		"category" : $('#meeting-desc').val(),
+		"deadline" : $('#limit-date').val(),
+		"sdate" : dateList[0],
+		"edate" : dateList[1],
+		"content" : $('#meeting-content').val(),
+		"photo" : $('#photo-path').val()
+	}
 
-			$
-					.post(serverRoot + '/meeting/add.json', param,
-							function(ajaxResult) {
-								if (ajaxResult.status != "success") {
-									alert(ajaxResult.data);
-									return;
-								}
-								var meeting = JSON.stringify(ajaxResult.data);
-								location.href = '../meetmain/meetmain.html'
-										+ '?member='
-										+ JSON.parse(meeting).meetBossNo
-										+ '&meetingNo='
-										+ JSON.parse(meeting).meetingNo;
-							}, 'json');
+	$.post(serverRoot + '/meeting/add.json', param,function(ajaxResult) {
+		if (ajaxResult.status != "success") {
+			alert(ajaxResult.data);
+			return;
+		}
+		var meeting = JSON.stringify(ajaxResult.data);
+		location.href = '../meetmain/meetmain.html'
+				+ '?member='
+				+ JSON.parse(meeting).meetBossNo
+				+ '&meetingNo='
+				+ JSON.parse(meeting).meetingNo;
+	}, 'json');
 
-			// $.post(serverRoot + '/meeting/add.json', param,
-			// function(ajaxResult) {
-			// if (ajaxResult.status != "success") {
-			// alert(ajaxResult.data);
-			// return;
-			// }
-			// location.href = '../meetmain/meetmain.html';
-			// }, 'json');
-
-		});
+});
 
 // 이미지 선택 시 파일 업로드
-$('#photo').fileupload(
-		{
-			url : serverRoot + '/common/fileupload.json',
-			dataType : 'json',
-			sequentialUploads : true,
-			singleFileUploads : false,
-			autoUpload : true,
-			disableImageResize : /Android(?!.*Chrome)|Opera/
-					.test(window.navigator && navigator.userAgent),
-			previewMaxWidth : 60,
-			previewMaxHeight : 60,
-			previewCrop : true,
-			done : function(e, data) {
-				/* console.log(data.result); */
-				$('#photo-path').val(data.result.data[0]);
-			},
-			processalways : function(e, data) {
-				console.log('fileuploadprocessalways()...', data.files.length,
-						data.index);
-				var img = $('#photo-img');
-				if (data.index == 0) {
-					var canvas = data.files[0].preview;
-					var dataURL = canvas.toDataURL();
-					/*console.log(canvas);*/
-					img.attr('src', dataURL).css('width', '100px');
-					$('#photo-label').css('display', '');
-				}
-			}
-		});
+$('#photo').fileupload({
+	url : serverRoot + '/common/fileupload.json',
+	dataType : 'json',
+	sequentialUploads : true,
+	singleFileUploads : false,
+	autoUpload : true,
+	disableImageResize : /Android(?!.*Chrome)|Opera/
+			.test(window.navigator && navigator.userAgent),
+	previewMaxWidth : 60,
+	previewMaxHeight : 60,
+	previewCrop : true,
+	done : function(e, data) {
+		$('#photo-path').val(data.result.data[0]);
+	},
+	processalways : function(e, data) {
+/*		console.log('fileuploadprocessalways()...', data.files.length,
+				data.index);*/
+		var img = $('#photo-img');
+		if (data.index == 0) {
+			var canvas = data.files[0].preview;
+			var dataURL = canvas.toDataURL();
+			/*console.log(canvas);*/
+			img.attr('src', dataURL).css('width', '100px');
+			$('#photo-label').css('display', '');
+		}
+	}
+});
