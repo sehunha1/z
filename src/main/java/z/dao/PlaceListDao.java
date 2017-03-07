0b40da9b33eeaa3beb244965ba18165d637c2cb0
WooJong Kim <skimwooj@gmail.com>
2017-03-07 10:55:43 +0900

    .

--- src/main/java/z/dao/PlaceListDao.java
+++ src/main/java/z/dao/PlaceListDao.java
@@ -4,6 +4,5 @@ import z.domain.PlaceList;
 
 public interface PlaceListDao {
   int insert(PlaceList placelist) throws Exception;
-  int countP(String place) throws Exception;
-  int countA(String address) throws Exception;
+  int count(String address) throws Exception;
 }
