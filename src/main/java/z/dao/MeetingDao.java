0a3f9ee6c77014845e118535020aea247add5952
jned0317 <jned0317@naver.com>
2017-03-09 19:15:20 +0900

    .

--- src/main/java/z/dao/MeetingDao.java
+++ src/main/java/z/dao/MeetingDao.java
@@ -18,7 +18,7 @@ public interface MeetingDao {
   int insertTime(Meeting meeting) throws Exception; // 방 개설시 time 테이블 데이터 삽입
   int insertLink(Meeting meeting) throws Exception; // 방 개설시 link 테이블 데이터 삽입
   ArrayList<Meeting> getListMeetingCards(int memberNo) throws Exception; // 모임 리스트 가져오기
-  Meeting getDetailMeeting(int memberNo, int meetingNo) throws Exception; // 완료 모임 상세 정보 가져오기
+  Meeting getDetailMeeting(@Param("memberNo") int memberNo, @Param("meetingNo") int meetingNo) throws Exception; // 완료 모임 상세 정보 가져오기
   Meeting getOneMeeting(int meetingNo) throws Exception;
   List<Board> meetBoardList (int meetingNo) throws Exception; // 완료 모임 게시글 정보 가져오기
   List<Board> keywordBoardList (@Param("meetingNo") int meetingNo, @Param("keyWord") String keyWord) throws Exception; // 완료 모임 게시글 정보 가져오기
