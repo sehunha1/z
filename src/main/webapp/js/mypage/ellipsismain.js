11dd45152452bc695717638892ad320c7c767468
WooJong Kim <skimwooj@gmail.com>
2017-03-24 15:59:13 +0900

    .

---
+++ src/main/webapp/js/mypage/ellipsismain.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Script file to handle click event of demo page and to invoke ellipsis plugin method.
+ * 
+ * **/
+
+(function($) {
+		$(".test1").ellipsis(20);
+		$(this).hide();
+})(jQuery);
