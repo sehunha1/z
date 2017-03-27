fa0c5b59d76df79eb47c21c984ae5955590cbf98
hase <hase@haseMac.local>
2017-03-27 11:10:24 +0900

    .

--- src/main/webapp/js/meetmain/meetmain.js
+++ src/main/webapp/js/meetmain/meetmain.js
@@ -2,6 +2,8 @@ var urlMemberNo = window.location.search.split("&")[0].substring(10);
 var meetingNo = window.location.search.split("&")[1].substring(10); // 모임 번호
 var sessionMemberNo = JSON.parse(window.sessionStorage.getItem("member")).memberNo;
 
+if (sessionMemberNo == null) sessionMemberNo = 0;
+
 if (urlMemberNo != sessionMemberNo) {
     swal({title: "잘못된 접근입니다.",
           text: "1초후 내모임리스트로 이동합니다.",
